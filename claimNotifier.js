0d143d007f9b2078409818b007e476b3bb58057e
Leo <leobensman@gmail.com>
2023-11-08 14:50:15 -0500

    0_2 changes

--- claimNotifier.js
+++ claimNotifier.js
@@ -86,47 +86,21 @@ async function sendWhatsappMessage(to, message) {
 
 async function sendEmail(to, message) {
   try {
-    fetch('https://api.sendgrid.com/v3/mail/send', {
+    const form = new FormData();
+    form.append('from', 'Excited User <' + config.mailgun.email_address + '>');
+    form.append('to', config.mailgun.email_address);
+    form.append('to', to);
+    form.append('subject', 'New message from "Whos In?"');
+    form.append('text', message);
+
+    fetch('https://api.mailgun.net/v3/' + config.mailgun.domain + '/messages', {
       method: 'POST',
       headers: {
-        'Authorization': '',
-        'Content-Type': 'application/json'
+        'Authorization': 'Basic ' + btoa(config.mailgun.auth_token)
       },
-      // body: '{"personalizations": [{"to": [{"email": "test@example.com"}]}],"from": {"email": "test@example.com"},"subject": "Sending with SendGrid is Fun","content": [{"type": "text/plain", "value": "and easy to do anywhere, even with cURL"}]}',
-      body: JSON.stringify({
-        'personalizations': [
-          {
-            'to': [
-              {
-                'email': 'test@example.com'
-              }
-            ]
-          }
-        ],
-        'from': {
-          'email': 'test@example.com'
-        },
-        'subject': 'Sending with SendGrid is Fun',
-        'content': [
-          {
-            'type': 'text/plain',
-            'value': 'and easy to do anywhere, even with cURL'
-          }
-        ]
-      })
-    }).then(response => {
-      if (!response.ok) {
-        throw new Error('HTTP error ' + response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data)
-    })
-    .catch(error => {
-      console.error('Fetch error:', error);
+      body: form
     });
-    console.log("-----------------------------------------------------fetched")
+
   } catch {
     console.log('error sending email')
   }
@@ -163,6 +137,9 @@ const signalReceived = new Promise((resolve) => {
                 if (contact.text_number.length > 0) {
                   sendText(contact.text_number, textMessage)
                 }
+                if (contact.email.length > 0) {
+                  sendEmail(contact.email, textMessage)
+                }
                 if (contact.whatsapp_number.length > 0) {
                   sendWhatsappMessage(contact.whatsapp_number, textMessage)
                 }
@@ -179,14 +156,14 @@ const signalReceived = new Promise((resolve) => {
 });
 appWs.on("signal", signalCb);
 
-// sendEmail('test', 'test email')
+sendEmail('test', 'test email')
 
 await appWs.callZome({
   cell_id,
   zome_name: "notifications",
   fn_name: "claim_notifier",
   // provenance: agent_key,
-  payload: null,
+  payload: "Official notifier service",
 });
 
 await appWs.callZome({
@@ -203,3 +180,4 @@ await appWs.callZome({
   },
 });
 await signalReceived;
+
